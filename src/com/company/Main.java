703f6286161cf978ad9dddb9776a1dcf821a46d5
ChingizIM <funfulik.83@gmail.com>
2023-02-05 16:34:33 +0600

    Исправил ошибки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,34 @@ public class Main {
     public static String itog2 = "Оставайтесь дома";
 
     public static void main(String[] args) {
-        System.out.println(ageAndTemperature(23, 17));
-        System.out.println(ageAndTemperature(39, 33));
-        System.out.println(ageAndTemperature(7, 1));
-        System.out.println(ageAndTemperature(49, -11));
-        System.out.println(ageAndTemperature(26, 23));
+        System.out.println(ageAndTemperature(generateRandomAge(), 17) );
+        System.out.println(ageAndTemperature(generateRandomAge(), 33));
+        System.out.println(ageAndTemperature(generateRandomAge(), 1));
+        System.out.println(ageAndTemperature(generateRandomAge(), -11));
+        System.out.println(ageAndTemperature(generateRandomAge(), 23));
 
 
     }
 
     public static String ageAndTemperature(int age, int temperature) {
         if (age > 20 && age < 45 && temperature > -20 && temperature < 30) {
-            return generateRandomAge();
+            return itog;
         } else if (age < 20 && temperature > 0 && temperature < 28) {
-            return generateRandomAge();
+            return itog;
         } else if (age > 45 && temperature > -10 && temperature < 25) {
-            return generateRandomAge();
+            return itog;
         } else {
-            return generateRandomAge();
+            return itog2;
         }
     }
 
-    public static String generateRandomAge() {
+    public static int generateRandomAge() {
         int a = 10;
         int b = 23;
         int random = a + (int) (Math.random() * b);
-        System.out.println(random);
-        return itog;
+
+
+        return random;
     }
 
 }
